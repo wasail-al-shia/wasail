b346bdb4abb7e26cf3887f6e7d5b20ca4fb4511d
wasail <wasail.al.shia@gmail.com>
2024-04-13 15:29:39 -0500

    fix date fn

--- assets/js/utils/date.js
+++ assets/js/utils/date.js
@@ -4,7 +4,7 @@ export const formatIsoStrToLocal = (dtIsoStr) =>
     dateStyle: "short",
   });
 
-export const formatIsoStrToLocalDate = (dtIsoStr) =>
-  new Date(dtIsoStr + "Z").toLocaleString("en-US", {
-    dateStyle: "short",
-  });
+export const formatIsoStrToLocalDate = (dtIsoStr) => {
+  const dateObj = new Date(dtIsoStr + "T00:00:00");
+  return new Intl.DateTimeFormat("en-US").format(dateObj);
+};
